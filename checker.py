74f9adebcf5cebfd890a2585a2a095aab819201f
Kendrick Linux <kendricknguyen76@gmail.com>
2024-12-21 14:53:16 -0800

    Collects all followers from follower json

--- checker.py
+++ checker.py
@@ -20,6 +20,18 @@ def get_following(following_path : Path) -> list[str]:
 		return following_list
 
 
+def get_follower(follower_path : Path) -> list[str]:
+	with open(follower_path, "r") as follower_json:
+		follower_return_list = []
+		follower_file_list = json.load(follower_json)
+
+		for section in follower_file_list:
+			follower_return_list.append(section["string_list_data"][0]["value"])
+
+		return follower_return_list
+
+
 if __name__ == "__main__":
 	print(len(get_following(TEST_FOLLOWING)))
+	print(len(get_follower(TEST_FOLLOWERS)))
 	
