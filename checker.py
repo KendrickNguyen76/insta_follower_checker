80e51502bd993f71689321e07d8bb1797d301e21
Kendrick Linux <kendricknguyen76@gmail.com>
2024-12-21 14:47:08 -0800

    Create list of following from json file

--- checker.py
+++ checker.py
@@ -11,9 +11,15 @@ TEST_FOLLOWERS = Path("followers_and_following/followers_1.json")
 
 def get_following(following_path : Path) -> list[str]:
 	with open(following_path, "r") as following_json:
+		following_list = []
 		following_dict = json.load(following_json)
 
-		print(following_dict)
+		for section in following_dict["relationships_following"]:
+			following_list.append(section["string_list_data"][0]["value"])
+
+		return following_list
+
 
 if __name__ == "__main__":
-	get_following(TEST_FOLLOWING)
+	print(len(get_following(TEST_FOLLOWING)))
+	
